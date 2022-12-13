cf43fc5e27bb69557b927008717075cfd435919f
Deja Clarke (dejclark) <dejclark@cisco.com>
2022-12-13 15:32:31 -0800

    post method

--- my-app/src/services/todoServices.js
+++ my-app/src/services/todoServices.js
@@ -1,10 +1,31 @@
 const baseUrl = `${process.env.REACT_APP_API_URL}/todos`;
 
 // Fetch todos from url then return response as json
-export const loadTodos = () => {
-    return fetch(baseUrl).then((res) => res.json());
+export const loadTodos = async () => {
+    const res = await fetch(baseUrl);
+    return await res.json();
 }
 // Fetch todos by id
-export const getTodo = (id) => {
-    return fetch(`${baseUrl}/${id}`).then((res) => res.json);
+export const getTodo = async (id) => {
+    const res = await fetch(`${baseUrl}/${id}`);
+    return res.json;
+}
+
+// Create todo using put method
+export const createTodo = async (todo) => {
+    const res = await fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: todo.title,
+            completed: todo.completed
+        }),
+    });
+    return await res.json();
+}
+
+export const updateTodo = (todo) => {
+
 }
