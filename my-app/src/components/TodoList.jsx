733809df2ef2eb77e2644d35eea69835b43e05b3
Deja Clarke (dejclark) <dejclark@cisco.com>
2022-12-13 22:43:16 -0800

    states created

--- my-app/src/components/TodoList.jsx
+++ my-app/src/components/TodoList.jsx
@@ -3,3 +3,10 @@ import { Tabs, Layout, Row, Col, Input, message} from 'antd';
 import './TodoList.css'
 import './TodoForm'
 import { createTodo, deleteTodo, loadTodos, updateTodo }from './services/todoServices'
+
+
+const TodoList = () => {
+    // States
+    const [todos, setTodos] = useState([]);
+    const [notDoneTodos, setNotDoneTodos] = useState([]);
+}
